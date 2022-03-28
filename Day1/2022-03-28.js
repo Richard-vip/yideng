68d67a016a84fed662cee96b4d0ec7842de29735
wangrenjie03 <wangrenjie03@kuaishou.com>
2022-03-28 18:57:42 +0800

    feat: 2022-03-28

---
+++ Day1/2022-03-28.js
@@ -0,0 +1,59 @@
+/**
+ * leetcode 989 数组形式的整数方法
+ * 输入：num = [1,2,0,0], k = 34
+ * 输出：[1,2,3,4]
+ * 解释：1200 + 34 = 1234
+ */
+
+function addToArrayForm(num, k) {
+  const n = num.length;
+  const res = [];
+  for (let i = n - 1; i >= 0 || k > 0; --i, k = Math.floor(k / 10)) {
+    if (i >= 0) {
+      k += num[i];
+    }
+    res.push(k % 10);
+  }
+  res.reverse();
+  return res;
+}
+
+/**
+ * 手写函数柯里化
+ * 
+function curry(func) {
+  //此处补全
+}
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+let curriedSum = curry(sum);
+
+alert(curriedSum(1, 2, 3)); // 6, still callable normally
+alert(curriedSum(1)(2, 3)); // 6, currying of 1st arg
+alert(curriedSum(1)(2)(3)); // 6, full currying
+
+ */
+
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
+  //此处补全
+}
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+let curriedSum = curry(sum);
+
+alert(curriedSum(1, 2, 3)); // 6, still callable normally
+alert(curriedSum(1)(2, 3)); // 6, currying of 1st arg
+alert(curriedSum(1)(2)(3)); // 6, full currying
