7ab6ff1aba5f7c9b215624038ba174951ce63379
wangrenjie03 <wangrenjie03@kuaishou.com>
2022-03-30 19:17:23 +0800

    feat: day3 leetcode

---
+++ Day3/2022-03-30.js
@@ -0,0 +1,58 @@
+/**
+ * leetcode [1381] 设计一个支持增量操作的栈 https://leetcode-cn.com/problems/design-a-stack-with-increment-operation/description/
+ * 题解：
+ *
+ *
+ * Your CustomStack object will be instantiated and called as such:
+ * var obj = new CustomStack(maxSize)
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * obj.increment(k,val)
+ */
+
+/**
+ * @param {number} maxSize
+ */
+var CustomStack = function (maxSize) {
+  this.data = [];
+  this.maxSize = maxSize;
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+CustomStack.prototype.push = function (x) {
+  if (this.data.length < this.maxSize) {
+    this.data.push(x);
+  }
+};
+
+/**
+ * @return {number}
+ */
+CustomStack.prototype.pop = function () {
+  if (!!this.data.length) {
+    return this.data.pop();
+  } else {
+    return -1;
+  }
+};
+
+/**
+ * @param {number} k
+ * @param {number} val
+ * @return {void}
+ */
+CustomStack.prototype.increment = function (k, val) {
+  let i = 0;
+  while (i < k) {
+    if (i < this.data.length) {
+      this.data[i] += val;
+    }
+    ++i;
+  }
+};
+
+
+
